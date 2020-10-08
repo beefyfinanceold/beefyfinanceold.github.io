7a5d66bb316a674726e1216f47e1d5a9b99ded48
superbeefyboy <superbeefyboy@pm.me>
2020-10-08 16:02:46 -0300

    withdraw from burning strats backup

---
+++ precache-manifest.477cdb2be365ff88ec882a89e41bb927.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb9e2a91abcb856ee99ebfa5258b339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32a0855a31e80340a8e6",
+    "url": "/static/css/main.c276258c.chunk.css"
+  },
+  {
+    "revision": "8ed8f38ad3801320a27e",
+    "url": "/static/js/2.06ccdddf.chunk.js"
+  },
+  {
+    "revision": "32a0855a31e80340a8e6",
+    "url": "/static/js/main.2ee1d7a1.chunk.js"
+  },
+  {
+    "revision": "a1d1ff2ccae6b2eb0cd4",
+    "url": "/static/js/runtime-main.48f7e4d2.js"
+  },
+  {
+    "revision": "4e504f3e4a773535a043747c5db396d8",
+    "url": "/static/media/BIFI-logo.4e504f3e.svg"
+  },
+  {
+    "revision": "d569caeed9945646295904122bd90f2c",
+    "url": "/static/media/BUSD-logo.d569caee.png"
+  },
+  {
+    "revision": "37e4f26b52b7f42f079da7cd30eeaf96",
+    "url": "/static/media/ETH-logo.37e4f26b.png"
+  },
+  {
+    "revision": "f66b0cf3ab573715ce7fadb693f9702b",
+    "url": "/static/media/LINK-logo.f66b0cf3.png"
+  },
+  {
+    "revision": "4d06ed03ee9590d36fd7f81e10d97093",
+    "url": "/static/media/USDT-logo.4d06ed03.png"
+  },
+  {
+    "revision": "4e504f3e4a773535a043747c5db396d8",
+    "url": "/static/media/beefy.4e504f3e.svg"
+  }
+]);
